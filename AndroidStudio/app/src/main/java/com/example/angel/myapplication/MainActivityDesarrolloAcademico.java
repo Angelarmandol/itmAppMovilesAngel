54183358a38eb16027f5b097571fa659045896ce
Angelarmandol <Angelarmandol@hotmail.com>
2018-10-22 02:21:03 -0700

    facebool login success

--- AndroidStudio/app/src/main/java/com/example/angel/myapplication/MainActivityDesarrolloAcademico.java
+++ AndroidStudio/app/src/main/java/com/example/angel/myapplication/MainActivityDesarrolloAcademico.java
@@ -1,5 +1,6 @@
 package com.example.angel.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,12 +14,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivityDesarrolloAcademico extends AppCompatActivity
 
         implements NavigationView.OnNavigationItemSelectedListener {
     private WebView mWebview;
+    private TextView info;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +38,7 @@ public class MainActivityDesarrolloAcademico extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -45,10 +50,42 @@ public class MainActivityDesarrolloAcademico extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_main_desarrollo_academico);
+        info = (TextView)findViewById(R.id.textViewFacebook);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
 
 
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
 
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -65,6 +102,12 @@ public class MainActivityDesarrolloAcademico extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_activity_desarrollo_academico, menu);
         return true;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -87,7 +130,9 @@ public class MainActivityDesarrolloAcademico extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Toast toast2 =
+                Toast.makeText(getApplicationContext(), "Selected: "+item.getItemId(), Toast.LENGTH_SHORT);
+        toast2.show();
         if (id == R.id.nav_config) {
 
             Toast toast3 =
