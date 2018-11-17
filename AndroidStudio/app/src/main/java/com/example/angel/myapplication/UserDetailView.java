c3ab5bc33b64bdbb51f259eeb4edacc869b0c9dd
Angelarmandol <angelarmandol@hotmail.com>
2018-11-16 16:02:31 -0800

    deleted llave png

--- AndroidStudio/app/src/main/java/com/example/angel/myapplication/UserDetailView.java
+++ AndroidStudio/app/src/main/java/com/example/angel/myapplication/UserDetailView.java
@@ -3,7 +3,7 @@ package com.example.angel.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class UserDetail extends AppCompatActivity {
+public class UserDetailView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
