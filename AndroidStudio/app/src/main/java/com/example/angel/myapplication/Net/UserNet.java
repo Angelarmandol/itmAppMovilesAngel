229998f407713ade9047608d0a333e0f6b1ae118
Angelarmandol <Angelarmandol@hotmail.com>
2018-11-19 19:44:04 -0800

    ready for adapter

--- AndroidStudio/app/src/main/java/com/example/angel/myapplication/Net/UserNet.java
+++ AndroidStudio/app/src/main/java/com/example/angel/myapplication/Net/UserNet.java
@@ -75,8 +75,6 @@ public class UserNet {
         myRef.addValueEventListener(new ValueEventListener() {
 
 
-
-
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Log.i("normal ase", "val: "+dataSnapshot.getValue());
@@ -86,13 +84,18 @@ public class UserNet {
                     list_Materias.put(snapshot,dataSnapshot.getValue());
                     localArrayList.add(dataSnapshot.getChildren());
                     test2.put(snapshot, dataSnapshot);
-                    adaptador.add(dataSnapshot.toString());
+                   //adaptador.add(test2.toString());
+                    adaptador.add("----");
 
 
                 }
 
                 Log.i("mandar","snapshot");
                 jSONParsin(test2);
+
+
+
+
                 //Log.i("nombres as red:"," "+list_Materias.toString());
 
                 holder.setHolder(list_Materias.toString());
@@ -132,8 +135,10 @@ String text;
 
     public void jSONParsin(HashMap test){
 
-        ArrayList<UserDetail> detailUsers= new ArrayList<UserDetail>();
+        ArrayList<ArrayList> detailUsers= new ArrayList<ArrayList>();
         ArrayList<String>datos = new ArrayList<String>();
+
+
         String conv = test.toString();
         String[] fff = conv.split("val: ");
 
@@ -143,86 +148,65 @@ String text;
         System.out.println("------");
         String source = fff[0];
         String[] users = source.split("DataSnapshot");
-/////del 1 en adelante
+
         System.out.println("1"+users[1]);
 
         System.out.println("Empieza separaciono0o0o0o0ooo00ooo0o");
+            int iteraror=0;
+            for(int x=1; x<24; x++){
 
-        for(int x=1; x<24; x++){
+            String id = users[x].substring(9);
+            String[] id2 = id.split(", ");
+            System.out.println("id2:"+id2[0]);
+            datos.add(0,id2[0]);
+            /////////////////////////////
+            String[] telefono = users[x].split("tv_telephone=");
+            telefono=telefono[1].split(", ");
+            System.out.println("valor de thelepohe:"+telefono[0]);
+            datos.add(1,telefono[0]);
+            /////////////////////////////
+            String[] score = users[x].split("score=");
+            score=score[1].split(", ");
+            System.out.println("valor de score:"+score[0]);
+            datos.add(2,score[0]);
+            ////////////////////////////
+            String[] area = users[x].split("area_resources=");
+            area=area[1].split(", ");
+            System.out.println("valor de area:"+area[0]);
+            datos.add(3,area[0]);
+            ////////////////////////////
+            String[] name = users[x].split("fbUserName=");
+            name=name[1].split(", ");
+            System.out.println("valor de name:"+name[0]);
+            datos.add(4,name[0]);
+            ////////////////////////////
+            String[] link = users[x].split("link=");
+            link=link[1].split(", ");
+            System.out.println("valor de link:"+link[0]);
+            datos.add(5,link[0]);
+            ////////////////////////////
+            String[] tv_email = users[x].split("tv_email=");
+            tv_email=tv_email[1].split(", ");
+            System.out.println("valor de tv_email:"+tv_email[0]);
+            datos.add(6,tv_email[0]);
+            System.out.println("..............................................................");
+            detailUsers.add(iteraror,datos);
+            iteraror++;
 
-        String id = users[x].substring(9);
-        String[] id2 = id.split(", ");
-        System.out.println("id2:"+id2[0]);
-        datos.add(0, id2[0]);
-        /////////////////////////////
-        String[] telefono = users[x].split("tv_telephone=");
-        telefono=telefono[1].split(", ");
-        System.out.println("valor de thelepohe:"+telefono[0]);
-        /////////////////////////////
-        String[] score = users[x].split("score=");
-        score=score[1].split(", ");
-        System.out.println("valor de score:"+score[0]);
-        ////////////////////////////
-        String[] area = users[x].split("area_resources=");
-        area=area[1].split(", ");
-        System.out.println("valor de area:"+area[0]);
-        ////////////////////////////
-        String[] name = users[x].split("fbUserName=");
-        name=name[1].split(", ");
-        System.out.println("valor de name:"+name[0]);
-        ////////////////////////////
-        String[] link = users[x].split("link=");
-        link=link[1].split(", ");
-        System.out.println("valor de link:"+link[0]);
-        ////////////////////////////
-        String[] tv_email = users[x].split("tv_email=");
-        tv_email=tv_email[1].split(", ");
-        System.out.println("valor de tv_email:"+tv_email[0]);
-
-        System.out.println("..............................................................");
-x++;
+            x++;
 
         }
 
-/*
 
-        JSONArray lista = null;
-        try {
-            lista = json.getJSONArray("568749");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        for(int i = 0; i < lista.length(); i++){
-            UserDetail conditions = new UserDetail();
-            JSONObject c = null;
-            try {
-                c = lista.getJSONObject(i);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-                /*
-                JSONObject phone = c.getJSONObject("main");
-                JSONArray wt = c.getJSONArray("weather");
-                conditions.weatherSimple =wt.getJSONObject(0).getString("main");
-                conditions.dateComplete = c.getString("dt_txt");
-                String kelvin = phone.getString("temp");
-                String subString = kelvin.toString();
-                conditions.temp = (subString);
-                String idIcon = wt.getJSONObject(0).getString("id");
-                conditions.imageUrl=getUrlIcon(idIcon);
-                System.out.println(conditions.imageUrl);
-                conditions.humidity = phone.getString("humidity")+"%";
-                conditions.pressure = phone.getString("pressure")+" bar";
-                JSONObject ws = c.getJSONObject("wind");
-                conditions.windSpeed =ws.getString("speed");
-
+        System.out.println("For de detailusers");
+        System.out.println(detailUsers);
+            for(int v=0; v<detailUsers.size();v++){
+                System.out.println(".-.-.-..-"+v);
 
 
         }
 
- */
+
 
     }
 
